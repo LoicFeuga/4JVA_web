9867f9123101953a24d40dddda7bde7bad0207f7
BXXX\user <loic.feuga@gmail.com>
2017-01-18 15:48:36 +0100

    Web Service qui marche plus avec appelle EJB Les 27 et 28 sont à inverser en commentaire pour que ça marche

--- src/com/supinfo/webservices/UserService.java
+++ src/com/supinfo/webservices/UserService.java
@@ -1,18 +1,21 @@
 package com.supinfo.webservices;
 
+import javax.ejb.EJB;
 import javax.jws.WebService;
 import javax.ws.rs.FormParam;
 import javax.ws.rs.POST;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 
+import com.supinfo.interfaces.InterfacesDao;
 import com.supinfo.servlet.Index;
 
 @WebService
 @Path("/users")
 public class UserService{
 
-
+	@EJB
+	InterfacesDao dao;
 
 		
     @POST
@@ -20,8 +23,9 @@ public class UserService{
 	@Produces("application/json")
     public String login(@FormParam("login") String login, @FormParam("mdp") String mdp) {
     	mdp = Index.aes_encrypt(mdp);
-    	Boolean logged = true;//= dao.login(login, mdp);
     	
+    	//Boolean logged = true;
+    	Boolean logged = dao.login(login, mdp);
     	
     	return logged == null ? "false" : logged+"";
 
